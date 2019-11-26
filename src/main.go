dea476b92d8bcfcea5bbc6bfad22cad6f25e6772
key kim <keyhyuk.kim@gmail.com>
2019-11-26 22:57:20 +0900

    Go simple server

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 	"time"
 )
@@ -13,12 +14,17 @@ func main() {
 	loggerInstance = log.New(os.Stdout, "INFO: ", log.LstdFlags)
 	loggerInstance.Print("Log service started")
 
-	for {
-		go func(url string) {
-			loggerInstance.Print("Fetching from " + url)
-		}("https://test.com")
+	http.Handle("/healthcheck", new(helloworldHandler))
+	http.ListenAndServe(":8080", nil)
+	loggerInstance.Print(time.Now()) // logging by 3 sec
+}
 
-		loggerInstance.Print(time.Now())
-		time.Sleep(3 * time.Second) // logging by 3 sec
-	}
+type helloworldHandler struct {
+	http.Handler
+}
+
+func (h *helloworldHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	loggerInstance.Print("HTTP Request reached")
+	message := "HelloWorld!\n"
+	w.Write([]byte(message))
 }
